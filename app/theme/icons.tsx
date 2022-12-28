d6406626981a81b6cdb59752590549477e08bca4
CaCaBlocker <Treeboy2021@outlook.com>
2022-12-28 23:11:53 +0900

    add: icons

--- app/theme/icons.tsx
+++ app/theme/icons.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Image, StyleSheet} from 'react-native';
+import {Image} from 'react-native';
 import IoniconsIcon from 'react-native-vector-icons/Ionicons';
 import FeatherIcon from 'react-native-vector-icons/Feather';
 import AntDesignIcon from 'react-native-vector-icons/AntDesign';
@@ -9,82 +9,99 @@ import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIc
 
 import Images from 'app/theme/images';
 
-interface IconProps {
+interface FontIconProps {
   color?: string;
   size?: number;
 }
 
-export const CoinsIcon: React.FC<IconProps> = ({color, size}) => (
+interface ImageIconProps {
+  size: number;
+}
+
+export const CoinsIcon: React.FC<FontIconProps> = ({color, size}) => (
   <FontAwesomeIcon name="coins" color={color} size={size} />
 );
 
-export const GraphPieIcon: React.FC<IconProps> = ({color, size}) => (
+export const GraphPieIcon: React.FC<FontIconProps> = ({color, size}) => (
   <FoundationIcon name="graph-pie" color={color} size={size} />
 );
 
-export const AccessPointIcon: React.FC<IconProps> = ({color, size}) => (
+export const AccessPointIcon: React.FC<FontIconProps> = ({color, size}) => (
   <MaterialCommunityIcon name="access-point" color={color} size={size} />
 );
 
-export const SearchIcon: React.FC<IconProps> = ({color, size}) => (
+export const SearchIcon: React.FC<FontIconProps> = ({color, size}) => (
   <IoniconsIcon name="search" color={color} size={size} />
 );
 
-export const ExportIcon: React.FC<IconProps> = ({color, size}) => (
+export const ExportIcon: React.FC<FontIconProps> = ({color, size}) => (
   <FeatherIcon name="upload" color={color} size={size} />
 );
 
-export const FavoriteIcon: React.FC<IconProps> = ({color, size}) => (
+export const FavoriteIcon: React.FC<FontIconProps> = ({color, size}) => (
   <FeatherIcon name="star" color={color} size={size} />
 );
 
-export const BackIcon: React.FC<IconProps> = ({color, size}) => (
+export const BackIcon: React.FC<FontIconProps> = ({color, size}) => (
   <IoniconsIcon name="chevron-back-outline" color={color} size={size} />
 );
 
-export const UpIcon: React.FC<IconProps> = ({color, size}) => (
+export const UpIcon: React.FC<FontIconProps> = ({color, size}) => (
   <AntDesignIcon name="caretup" color={color} size={size} />
 );
 
-export const DownIcon: React.FC<IconProps> = ({color, size}) => (
+export const DownIcon: React.FC<FontIconProps> = ({color, size}) => (
   <AntDesignIcon name="caretdown" color={color} size={size} />
 );
 
-export const PlusIcon: React.FC<IconProps> = ({color, size}) => (
+export const PlusIcon: React.FC<FontIconProps> = ({color, size}) => (
   <AntDesignIcon name="plus" color={color} size={size} />
 );
 
-export const EllipsisIcon: React.FC<IconProps> = ({color, size}) => (
+export const EllipsisIcon: React.FC<FontIconProps> = ({color, size}) => (
   <AntDesignIcon name="ellipsis1" color={color} size={size} />
 );
 
-export const BellIcon: React.FC<IconProps> = ({color, size}) => (
+export const BellIcon: React.FC<FontIconProps> = ({color, size}) => (
   <MaterialCommunityIcon name="bell" color={color} size={size} />
 );
 
-export const SwapVertIcon: React.FC<IconProps> = ({color, size}) => (
+export const SwapVertIcon: React.FC<FontIconProps> = ({color, size}) => (
   <IoniconsIcon name="swap-vertical" color={color} size={size} />
 );
 
-export const CommentIcon: React.FC<IconProps> = ({color, size}) => (
+export const CommentIcon: React.FC<FontIconProps> = ({color, size}) => (
   <FontAwesomeIcon name="comment" color={color} size={size} />
 );
 
-export const SyncIcon: React.FC<IconProps> = ({color, size}) => (
+export const SyncIcon: React.FC<FontIconProps> = ({color, size}) => (
   <AntDesignIcon name="sync" color={color} size={size} />
 );
 
-export const HeartIcon: React.FC<IconProps> = ({color, size}) => (
+export const HeartIcon: React.FC<FontIconProps> = ({color, size}) => (
   <FeatherIcon name="heart" color={color} size={size} />
 );
 
-export const BadgeIcon: React.FC = () => (
-  <Image source={Images.badge} style={styles.badge} />
+export const BadgeIcon: React.FC<ImageIconProps> = ({size}) => (
+  <Image source={Images.i_badge} style={{width: size, height: size}} />
 );
 
-const styles = StyleSheet.create({
-  badge: {
-    width: 16,
-    height: 16,
-  },
-});
+export const BagIcon: React.FC<ImageIconProps> = ({size}) => (
+  <Image source={Images.i_bag} style={{width: size, height: size}} />
+);
+
+export const BiArrowIcon: React.FC<ImageIconProps> = ({size}) => (
+  <Image source={Images.i_bi_arrow} style={{width: size, height: size}} />
+);
+
+export const RoundIcon: React.FC<ImageIconProps> = ({size}) => (
+  <Image source={Images.i_round} style={{width: size, height: size}} />
+);
+
+export const ChartIcon: React.FC<ImageIconProps> = ({size}) => (
+  <Image source={Images.i_chart} style={{width: size, height: size}} />
+);
+
+export const DiamondIcon: React.FC<ImageIconProps> = ({size}) => (
+  <Image source={Images.i_diamond} style={{width: size, height: size}} />
+);
--- app/theme/images.ts
+++ app/theme/images.ts
@@ -4,9 +4,13 @@ import AvatarImage from 'app/assets/avatar.png';
 import RoundButtonBgImage from 'app/assets/bg-round-button.png';
 import ProfileDefaultAvatarImage from 'app/assets/profile-avatar-default.png';
 import HeaderBgImage from 'app/assets/bg-header.png';
-import DiamondImage from 'app/assets/diamond.png';
+import DiamondImage from 'app/assets/i-diamond.png';
 import PriceListItemBgImage from 'app/assets/bg-price-list-item.png';
-import BadgeImage from 'app/assets/badge.png';
+import BadgeImage from 'app/assets/i-badge.png';
+import BagImage from 'app/assets/i-bag.png';
+import BiArrowImage from 'app/assets/i-biarrow.png';
+import ChartImage from 'app/assets/i-chart.png';
+import RoundImage from 'app/assets/i-round.png';
 
 const Images = {
   bg_gradient: GradientBgImage,
@@ -15,9 +19,13 @@ const Images = {
   bg_round_button: RoundButtonBgImage,
   profile_default_avatar: ProfileDefaultAvatarImage,
   bg_header: HeaderBgImage,
-  diamond: DiamondImage,
+  i_diamond: DiamondImage,
   bg_price_list_item: PriceListItemBgImage,
-  badge: BadgeImage,
+  i_badge: BadgeImage,
+  i_bag: BagImage,
+  i_bi_arrow: BiArrowImage,
+  i_chart: ChartImage,
+  i_round: RoundImage
 };
 
 export default Images;
