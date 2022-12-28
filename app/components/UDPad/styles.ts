ee5d3254c7cc2f1e587e4455ef7e3ffa5f5a6de0
CaCaBlocker <Treeboy2021@outlook.com>
2022-12-28 23:14:23 +0900

    add: UDPad Component

---
+++ app/components/UDPad/styles.ts
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  text: {
+    marginLeft: 3,
+  },
+});
+
+export default styles;
