5b935a80a02d4f7a2fd4517989e9d98026767237
CaCaBlocker <umamlikeyou@gmail.com>
2023-03-17 06:13:27 +0900

    res: update theme

--- app/theme/images.ts
+++ app/theme/images.ts
@@ -1,11 +1,6 @@
-import GradientBgImage from 'app/assets/bg-gradient.png';
-import MainBgImage from 'app/assets/bg-main.png';
 import AvatarImage from 'app/assets/avatar.png';
-import RoundButtonBgImage from 'app/assets/bg-round-button.png';
 import ProfileDefaultAvatarImage from 'app/assets/profile-avatar-default.png';
-import HeaderBgImage from 'app/assets/bg-header.png';
 import DiamondImage from 'app/assets/i-diamond.png';
-import PriceListItemBgImage from 'app/assets/bg-price-list-item.png';
 import BadgeImage from 'app/assets/i-badge.png';
 import BagImage from 'app/assets/i-bag.png';
 import BiArrowImage from 'app/assets/i-biarrow.png';
@@ -13,14 +8,9 @@ import ChartImage from 'app/assets/i-chart.png';
 import RoundImage from 'app/assets/i-round.png';
 
 const Images = {
-  bg_gradient: GradientBgImage,
-  bg_main: MainBgImage,
   avatar: AvatarImage,
-  bg_round_button: RoundButtonBgImage,
   profile_default_avatar: ProfileDefaultAvatarImage,
-  bg_header: HeaderBgImage,
   i_diamond: DiamondImage,
-  bg_price_list_item: PriceListItemBgImage,
   i_badge: BadgeImage,
   i_bag: BagImage,
   i_bi_arrow: BiArrowImage,
